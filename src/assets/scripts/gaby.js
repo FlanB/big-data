7d039ee283b22fa69c6fcf5f3b56d3872f70d899
Ckyre <ckyre.game@gmail.com>
2022-03-09 11:55:49 +0100

    Chart.js

--- src/assets/scripts/gaby.js
+++ src/assets/scripts/gaby.js
@@ -1,1 +1,26 @@
-console.log('gaby.js');
+const ctx = document.getElementById('chart').getContext('2d');
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+      label: "Objets trouvés",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  };
+
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: data
+});
