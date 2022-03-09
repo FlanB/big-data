a86e9b47e99296b2f19cf9dc502a0f2e507fd589
FlanB <clement.renou@mmibordeaux.com>
2022-03-09 10:18:36 +0100

    ✨ create people pages

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# big-data
+# vite-multipage-configuration (vanilla js)
+Install dependencies:
+```
+npm install
+```
+
+Run in Dev:
+```
+npm run dev
+```
+
+Build:
+```
+npm run build
+```
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-    root: 'src',
-    build: {
-        outDir: '../dist'
-    }
-}
+import path from "path";
+import glob from "glob";
+
+export default {
+  root: path.join(__dirname, "src"),
+  build: {
+    outDir: path.join(__dirname, "dist"),
+    rollupOptions: {
+      input: glob.sync(path.resolve(__dirname, "src", "*.html")),
+    },
+  },
+};
