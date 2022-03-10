9921ce2c6121896fa89a1f963cb8d1a4cafeb79d
Ckyre <ckyre.game@gmail.com>
2022-03-10 10:47:20 +0100

    Gabriel chart

--- src/assets/scripts/gaby.js
+++ src/assets/scripts/gaby.js
@@ -1,75 +1,79 @@
-let objets_redistribues = [];
-
-function IsSpentOneMonth(date1, date2)
+function DrawChart(data)
 {
-    let time_difference = date2.getTime() - date1.getTime();
+    {
+        const canvas = document.getElementById("chart");
+        document.body.removeChild(canvas);
+        document.body.innerHTML += "<canvas id='chart'></canvas>";
+    }
+    const canvas = document.getElementById("chart");
+    const ctx = canvas.getContext('2d');
+    
+    const set = {
+        labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"],
+        datasets: [{
+            label: "Objets trouvés",
+            data: data,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    };
+
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: set
+    });
+}
+
+function IsSpentOneMonth(date)
+{
+    let time_difference = new Date(Date.now()) - date.getTime();
     let days_difference = time_difference / (1000 * 60 * 60 * 24);
-    return days_difference >= 10;
+    return days_difference >= 30;
 }
 
-// Noms des gares et frequentation
-fetch("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&rows=3000&sort=date").then(response => response.text())
-.then(raw => {
-    const data = JSON.parse(raw);
-    for (let i = 0; i < data.records.length; i++) {
-        const record = data.records[i].fields;
+function FetchData(city, year, callback)
+{
+    console.log("Starting research!");
+    const endpoint = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&lang=fr&rows=10000&sort=date&refine.date=" + year + "&refine.gc_obo_gare_origine_r_name=" + city;
+    
+    fetch(endpoint).then(response => response.text())
+    .then(raw => {
+        const data = JSON.parse(raw);
+        let objets_par_mois = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let debug_list = [];
 
-        if(typeof record.gc_obo_date_heure_restitution_c === 'undefined')
+        for (let i = 0; i < data.records.length; i++)
         {
-            objets_redistribues[i] = {
-                nom_gare: record.gc_obo_gare_origine_r_name,
-                date_perte: record.date,
-                redistribue: IsSpentOneMonth(new Date(record.date), new Date(Date.now()))
-            };
+            const record = data.records[i].fields;
+            if(typeof record.gc_obo_date_heure_restitution_c === 'undefined') // si obj n'a pas été rendus
+            {
+                let date_perte = new Date(record.date);
+
+                if(IsSpentOneMonth(date_perte)) // si obj a été perdue il y a plus d'un mois
+                {
+                    objets_par_mois[date_perte.getMonth()]++;
+                }
+
+                debug_list.push({
+                    nom_gare: record.gc_obo_gare_origine_r_name,
+                    date_perte: record.date,
+                    nature_obj: record.gc_obo_nature_c
+                });
+            }
         }
-    }
 
-    console.log(objets_redistribues);
-})
-.catch(error => console.log(error));
+        callback(objets_par_mois);
 
-// Visualize datas with Char.js
-/*
-const ctx = document.getElementById('chart').getContext('2d');
-const data = {
-    labels: frequentation,
-    datasets: [{
-    label: "Objets trouvés",
-    data: frequentation,
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-    }]
-};
-
-const chart = new Chart(ctx, {
-    type: 'line',
-    data: data
-});
-
-
-
-
-// Trash code
-let tableau2 = [];
-for (let i = 0; i < 10; i++) {
-    let occur = -1;
-    for(let y = 0; y < tableau2.length; y++){
-        if(tableau2[y].nom_gare === objets[i]) {
-            occur = y;
-        }
-    }
-
-    if(occur == -1) {
-        tableau2[tableau2.length] = {
-            gare_nom: objets[i],
-            count: 1
-        }
-    }
-    else {
-        tableau2[y].count++;
-    }
+        console.log(debug_list);
+        console.log("Query end!");
+    })
+    .catch(error => console.log(error));
 }
 
-console.log(tableau2);
-*/
+const citySelect = document.getElementById("city-select");
+const yearSelect = document.getElementById("year-select");
+
+document.getElementById("start-btn").addEventListener("click", () => {
+    FetchData(citySelect.value, yearSelect.value, (data) => DrawChart(data));
+});
