1d291acf653e8d705064211bdaac9d64e40c6fc3
Ckyre <ckyre.game@gmail.com>
2022-03-10 12:47:46 +0100

    merge

--- src/assets/scripts/gaby.js
+++ src/assets/scripts/gaby.js
@@ -5,7 +5,7 @@ function DrawChart(data)
     const set = {
         labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"],
         datasets: [{
-            label: "Objets trouvés",
+            label: "Objets redistribués",
             data: data,
             fill: false,
             backgroundColor: 'rgb(75, 192, 192)',
