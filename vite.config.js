c108af5046ee2ebe5c39317c36ea62e9b3dffded
FlanB <clement.renou@mmibordeaux.com>
2022-03-12 12:11:44 +0100

    🚑️ esbuild2022

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import path from "path";
-import glob from "glob";
+import path from "path"
+import glob from "glob"
 
 export default {
-  root: path.join(__dirname, "src"),
-  build: {
-    outDir: path.join(__dirname, "dist"),
-    rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, "src", "*.html")),
-    },
-  },
-};
+    root: path.join(__dirname, "src"), build: {
+        outDir: path.join(__dirname, "dist"), rollupOptions: {
+            input: glob.sync(path.resolve(__dirname, "src", "*.html"))
+        }, target: "es2022",
+    }
+}
